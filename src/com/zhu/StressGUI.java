b804dc1265418bbea87f65bad08aa60600d2e23e
madkilly <251201337@qq.com>
2015-09-21 18:31:00 +0800

    1

--- src/com/zhu/StressGUI.java
+++ src/com/zhu/StressGUI.java
@@ -5,6 +5,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,64 +20,25 @@ public class StressGUI {
 	private  JButton StartTest;
 	private  JButton StopTest;
 	private  JButton CloseTest;
+	private  JButton Clear;
 	private  JTextArea consoleT;
 	private  JFrame f;
+	private Thread thread;
+	runinfo runflag;
+	final StressTesting ST;
 	
 	public StressGUI(){
 		//设置按钮
 		StartTest=new JButton("strat");
 		StopTest=new JButton("stop");
 		CloseTest=new JButton("close");
+		Clear=new JButton("clear");
 		
-		//设置监听器
-		StartTest.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				final StressTesting ST=new StressTesting(consoleT);
-				
-				Thread thread=new Thread(new Runnable(){
-					@Override
-					public void run() {
-						// TODO Auto-generated method stub
-				    	
-						ST.runssh();
-				    	
-				    	if(DEBUG=="true")
-				         System.out.println("start testing");
-					}});
-				thread.start();
-
-				
-			}
-			
-		});
+		consoleT=new JTextArea();
+		runflag=new runinfo();
+		ST=new StressTesting(consoleT);
 		
-		
-		
-		CloseTest.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-	            f.setVisible(false);         //设置窗口f不可见
-	            f.dispose();            //释放窗口及其子组件的屏幕资源
-	            System.exit(0);
-			}
-			
-		});
-		
-		StopTest.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				if(DEBUG=="true")
-				System.out.println("stop testing");
-			}
-			
-		});
+		StopTest.setEnabled(false);
 		
 		// 创建Frame对象
 		f = new JFrame();
@@ -96,11 +58,12 @@ public class StressGUI {
 		//controlpanel.setBounds(20, 300, 150, 50);
 		controlpanel.add(StartTest);// 默认垂直居中，间距为5
 		controlpanel.add(StopTest);
+		controlpanel.add(Clear);
 		controlpanel.add(CloseTest);
 		controlpanel.setBackground(Color.black);
 		
 		//设置控制台panel
-		consoleT=new JTextArea();
+		
 		monitorpanel.getViewport().add(consoleT);
 
 		
@@ -128,12 +91,84 @@ public class StressGUI {
 		layoutcontrol.weighty=1;
 		layoutcontrol.gridheight=0;
 		f.add(monitorpanel,layoutcontrol);
+		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 		// 设置Frame
 		f.setSize(800, 600);
 		f.setLocationRelativeTo(null);
 		f.setTitle("nginx图片压力测试");
 		f.setResizable(false);
 		f.setVisible(true);
+		
+		//设置监听器
+		//start
+		StartTest.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+				StopTest.setEnabled(true);
+				StartTest.setEnabled(false);
+				 thread=new Thread(new Runnable(){
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						runflag.setRunflag(true);
+						ST.runssh(runflag);				    	
+				    	if(DEBUG=="true")
+				         System.out.println("start testing");
+					}});
+				thread.start();
+
+				
+			}
+			
+		});
+		
+		
+		//close
+		CloseTest.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+	            f.setVisible(false);         //设置窗口f不可见
+	            f.dispose();            //释放窗口及其子组件的屏幕资源
+	            System.exit(0);
+			}
+			
+		});
+		
+		//stop
+		StopTest.addActionListener(new ActionListener(){
+
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(DEBUG=="true")
+				System.out.println("stop testing");
+				runflag.setRunflag(false);
+				StopTest.setEnabled(false);
+				StartTest.setEnabled(true);
+				ST.close();
+				
+			}
+			
+		});
+		
+		//clear
+		Clear.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				consoleT.setText("");
+			}
+			
+		});
+		
+
 	}
 	
 	
@@ -143,9 +178,23 @@ public class StressGUI {
 
 	}
 	
-	
+	public class runinfo{
+		private volatile boolean runflag = false;
+		
+		public runinfo(){
+			runflag=false;
+		}
 
+		public boolean getRunflag() {
+			return runflag;
+		}
+
+		public void setRunflag(boolean runflag) {
+			this.runflag = runflag;
+		} 
+	}
 
 	
 }
+
 
--- src/com/zhu/StressTesting.java
+++ src/com/zhu/StressTesting.java
@@ -11,32 +11,40 @@ import java.io.OutputStream;
 
 import javax.swing.JTextArea;
 
+import com.zhu.StressGUI.runinfo;
+
+import ch.ethz.ssh2.ChannelCondition;
 import ch.ethz.ssh2.Connection;  
 import ch.ethz.ssh2.Session;  
 import ch.ethz.ssh2.StreamGobbler; 
 
 public class StressTesting {
 	InputStream in;
+	InputStream inerr;
 	OutputStream out;
 	JTextArea ConsoleArea;
+    Connection conn;  
+    Session ssh; 
 	public StressTesting(JTextArea ConsoleArea){
 		this.ConsoleArea=ConsoleArea;
 	}
-	  public  void runssh() {  
+	  public  void runssh(runinfo runflag) {  
           
-	        String hostname = "172.20.8.116";  
+/*	        String hostname = "172.20.8.116"; 
 	        String username = "root";  
-	        String password = "ufsoft"; 
-/*		    String hostname = "192.168.219.128";  
+	        String password = "ufsoft"; */
+		    String hostname = "192.168.28.6";  
 	        String username = "root";  
-	        String password = "zhukai"; */
+	        String password = "zhukai"; 
 	        String line;
+	        byte[] buff = new byte[8192];
+	        int len;
 	        //指明连接主机的IP地址  
-	        Connection conn = new Connection(hostname);  
-	        Session ssh = null;  
+	         conn = new Connection(hostname);  
+	         ssh = null;  
 	        try {  
 	            //连接到主机  
-	        	ConsoleArea.append("连接主机");
+	        	ConsoleArea.append("连接主机\n");
 	            conn.connect();  
 	            //使用用户名和密码校验  
 	            boolean isconn = conn.authenticateWithPassword(username, password);  
@@ -45,29 +53,33 @@ public class StressTesting {
 	            }else{  
 	                //System.out.println("已经连接OK");  
 	                ssh = conn.openSession();
+	                ssh.requestPTY("bash");
 	                ssh.startShell();
 	    			in = ssh.getStdout();
+	    			inerr=ssh.getStderr();
 	    			out = ssh.getStdin();
 	                //使用多个命令用分号隔开   
 	               //ssh.execCommand("ps -aux|grep nginx|awk  '{print $3 \" \t \" $4}'");
 	               // ssh.execCommand("ls");
 	    	
-	    			String comString="ps\n";
+	    			//String comString=" ps -aux|grep nginx|awk  '{print $3 \" \t \" $4}'\n";
+	    			String comString="ps \n";
 	    			byte[] command=comString.getBytes();
 	    			out.write(command);
 	    			out.flush();
-         			comString="ls\n";
+/*         			comString="ls\n";
 	    			command=comString.getBytes();
 	    			out.write(command);
-	    			out.flush();
+	    			out.flush();*/
 	                //只允许使用一行命令，即ssh对象只能使用一次execCommand这个方法，多次使用则会出现异常  
 //	              ssh.execCommand("mkdir hb");  
 	                //将屏幕上的文字全部打印出来  
-	                InputStream  is = new StreamGobbler(in);  
+	                InputStream  is = new StreamGobbler(in); 
+	                InputStream  iserr = new StreamGobbler(inerr);  
 	                BufferedReader brs = new BufferedReader(new InputStreamReader(is));  
-	                while(true){  
-	                     line = brs.readLine();  
-	                    if(line==null){  
+	                while(true){  	                		    					
+	    					line = brs.readLine();
+	 	                    if(runflag.getRunflag()==false){  
 	                        break;  
 	                    }  
 	                    ConsoleArea.append(line+"\n");
@@ -80,15 +92,31 @@ public class StressTesting {
 	                  
 	            }  
 	            //连接的Session和Connection对象都需要关闭  
+	            if(in!=null&&out!=null&&ssh!=null&&conn!=null){ 
 	            in.close();
 	            out.close();
 	            ssh.close();  
 	            conn.close();  
+	            }
 	              
 	        } catch (IOException e) {  
 	            // TODO Auto-generated catch block  
 	            e.printStackTrace();  
 	        }  
 	          
-	    }  
+	    } 
+	  public void close(){
+          try {
+        	if(in!=null&&out!=null&&ssh!=null&&conn!=null){  
+			in.close();
+			out.close();
+			ssh.close();  
+			conn.close(); 
+        	}
+  		} catch (IOException e) {
+			// TODO Auto-generated catch block
+  			System.out.println("关闭连接出错");
+			e.printStackTrace();
+		}
+	  }
 }
